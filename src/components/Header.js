724ec645706d0812d6e8fa98ed5aa7cefef64728
Henry Sullivan <sullivanbhenry@gmail.com>
2023-02-10 13:53:58 -0800

    add image to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,11 @@
 import React from "react";
+import wineImage from "./../img/wine-row.jpeg";
 
 export default function Header() {
   return(
     <React.Fragment>
-      <h1>Henry's Wine Shop</h1>
+      <h1 style={{textAlign: "center"}}>Henry's Wine Shop</h1>
+      <img src={wineImage} alt='picture of wine' style={{width:"100%"}}/>
     </React.Fragment>
   );
 }
